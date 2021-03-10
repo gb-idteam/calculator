b3f1dd1e6f541cac8e844ed658376ca91803a619
Kirill Kuznetsov <kuznechik.86@mail.ru>
2021-03-10 23:57:42 +0300

    Init commit

---
+++ src/test/java/ru/systemairac/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.systemairac.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
