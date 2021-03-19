d920516cd6437ed47580bc3ce5640985efc0fcc2
Maxim Kryuchkov <7293121+observant-sun@users.noreply.github.com>
2021-03-19 23:13:35 +0300

    Нахождение температуры методом половинного деления

--- src/main/java/ru/systemairac/calculator/dto/PointDto.java
+++ src/main/java/ru/systemairac/calculator/dto/PointDto.java
@@ -90,12 +90,25 @@ public class PointDto {
                 return (234 * Math.log(pressureD)-1500.3) / (23.5- Math.log(pressureD));
         }
         if (builder.humidity!= null && builder.enthalpy!=null) {
-            Double temperature = 0.0;
+            double tHigh = 80, tLow = -60; // TODO: диапазон?
+
+            double eHigh, eLow;
+
+            enthalpy = builder.enthalpy;
+            humidity = builder.humidity;
+
+            eHigh = calcEnthalpyFromTemperatureAndHumidity(tHigh, humidity);
+            eLow = calcEnthalpyFromTemperatureAndHumidity(tLow, humidity);
+
+            if (enthalpy > eHigh || enthalpy < eLow)
+                throw new IllegalArgumentException("Энтальпия вне диапазона расчетов");
+
+            Double temperature = findTemp(tLow, tHigh, enthalpy);
 
             //Метод итераций temperature будет вычислена, если x1-x2 сравняются.
             pressureD = calcPressureD(temperature);
-            Double x1 = 0.6222* builder.humidity/100 *pressureD/(ATMOSPHERE_PRESSURE-builder.humidity/100*pressureD/1000);
-            Double x2 = (builder.enthalpy - 1.006 * temperature) / (2501 + 1.805 * temperature) / 1000;
+//            Double x1 = 0.6222* builder.humidity/100 *pressureD/(ATMOSPHERE_PRESSURE-builder.humidity/100*pressureD/1000);
+//            Double x2 = (builder.enthalpy - 1.006 * temperature) / (2501 + 1.805 * temperature) / 1000;
             //TODO???
         }
         if (builder.enthalpy!= null && builder.moistureContent!=null) {
@@ -111,6 +124,42 @@ public class PointDto {
          */
         this.pressureD = calcPressureD(this.temperature);
         return null;
+    }
+
+    private static final double HEAT_CAPACITY_AIR_DRY = 1006; // Дж / (кг * К)
+    private static final double HEAT_OF_FUSION_WATER_0 = 2501000; // Дж / кг
+    private static final double HEAT_OF_FUSION_WATER_DELTA = 2360; // Дж / (кг * К)
+    private static final double HEAT_CAPACITY_WATER_VAPOR = 1860; // Дж / (кг * К)
+    private static final double MOLAR_RATIO = 0.622;
+
+
+    private double calcEnthalpyFromTemperatureAndHumidity(double temperature, double humidity) {
+        double pressureSaturatedVapor = pressureSaturatedVapor(temperature);
+        return HEAT_CAPACITY_AIR_DRY * temperature + (HEAT_OF_FUSION_WATER_0 - HEAT_OF_FUSION_WATER_DELTA * temperature
+                + HEAT_CAPACITY_WATER_VAPOR * temperature) * MOLAR_RATIO * pressureSaturatedVapor * humidity
+                / (101325 - pressureSaturatedVapor);
+    }
+
+    private double pressureSaturatedVapor(double temperature) {
+        return 1000 * Math.exp((16.57 * temperature - 115.72) / (233.77 + 0.997 * temperature));
+    }
+
+    private double findTemp(double tLow, double tHigh, double enthalpy) {
+        final double EPSILON = 0.0001;
+        double tMid = tHigh + tLow;
+        while (tHigh - tLow > EPSILON) {
+            tMid = (tHigh + tLow)/2;
+            // eHigh > eLow -- всегда (?)
+            double eMid = calcEnthalpyFromTemperatureAndHumidity(tMid, humidity);
+            if (eMid == enthalpy) {
+                return tMid;
+            } else if (eMid < enthalpy) {
+                tLow = tMid;
+            } else {
+                tHigh = tMid;
+            }
+        }
+        return tMid;
     }
 
     private Double calcPressureD(Double t) {
