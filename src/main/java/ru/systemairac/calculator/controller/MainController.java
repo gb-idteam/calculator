a20267e2328e9c6c4575a0faf089e30c92de9a8f
Kirill Kuznetsov <kuznechik.86@mail.ru>
2021-03-22 10:29:53 +0300

    Добавил таблица увлажнителей

--- src/main/java/ru/systemairac/calculator/controller/MainController.java
+++ src/main/java/ru/systemairac/calculator/controller/MainController.java
@@ -11,13 +11,30 @@ import ru.systemairac.calculator.dto.InfoDto;
 import ru.systemairac.calculator.dto.PointDto;
 import ru.systemairac.calculator.dto.TechDataDto;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MainController {
-
+    private List<HumidifierDto> himidifiers = new ArrayList<>();
     @RequestMapping({"","/"})
     public String index(Model model){
+        // Тестовая dtoшка
+        HumidifierDto humidifierDto = HumidifierDto.builder()
+                .id(1L)
+                .articleNumber("1")
+                .electricPower(5)
+                .maxVaporOutput(20)
+                .phase(3)
+                .vaporPipeDiameter(25)
+                .numberOfCylinders(1)
+                .voltage(380)
+                .price(BigDecimal.valueOf(1000))
+                .build();
+        himidifiers.add(humidifierDto);
         model.addAttribute("infoDto", new InfoDto());
-        model.addAttribute("humidifierDto", new HumidifierDto());
+        model.addAttribute("himidifiers", himidifiers);
         model.addAttribute("techDataDto", new TechDataDto());
         return "calculator";
     }
--- src/main/java/ru/systemairac/calculator/dto/HumidifierDto.java
+++ src/main/java/ru/systemairac/calculator/dto/HumidifierDto.java
@@ -4,11 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class HumidifierDto {
+    private Long id;
     private String articleNumber;
+    private float electricPower;
+    private float maxVaporOutput;
+    private int phase;
+    private int voltage;
+    private int numberOfCylinders;
+    private int vaporPipeDiameter;
+    private BigDecimal price;
 }
