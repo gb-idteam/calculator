366a60e02f30da7978b450abcc782284781b9d82
Maxim Kryuchkov <7293121+observant-sun@users.noreply.github.com>
2021-03-27 17:36:44 +0300

    type для input для пароля должен быть password

--- src/main/java/ru/systemairac/calculator/controller/RegistrationController.java
+++ src/main/java/ru/systemairac/calculator/controller/RegistrationController.java
@@ -3,7 +3,6 @@ package ru.systemairac.calculator.controller;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.systemairac.calculator.dto.UserDto;
