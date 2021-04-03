652225c36375254ed6518f007aa035eab2175701
Kirill Kuznetsov <kuznechik.86@mail.ru>
2021-04-03 16:07:28 +0300

    Fix

--- src/main/java/ru/systemairac/calculator/repository/VaporDistributorRepository.java
+++ src/main/java/ru/systemairac/calculator/repository/VaporDistributorRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.systemairac.calculator.domain.humidifier.VaporDistributor;
 
 public interface VaporDistributorRepository extends JpaRepository<VaporDistributor,Long> {
-    public VaporDistributor findFirstByDiameterAndLeAndLengthLessThan(int diameter,int length);
+    public VaporDistributor findFirstByDiameterAndLengthLessThan(int diameter,int length);
 }
