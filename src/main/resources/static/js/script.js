cdc55b9f565f0d0e70b9ab9b0d313f4bb0bfba5c
Kirill Kuznetsov <kuznechik.86@mail.ru>
2021-04-04 18:31:37 +0300

    Сделал скрипт для вывода суммы увлажнителя и выбранных опций

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,7 +1,14 @@
+var idSelectHumidifier;
 $(document).ready(function(){
     $('input:radio[name=idSelectHumidifier]').change(function(){
+        idSelectHumidifier = $(this).val();
+        var price = priceHumidifier(idSelectHumidifier) + calcSummaryOption();
+        $("#Summary").text("Сумма: " + price);
 
-        var id = $(this).val();
+        //Для теста
+
+        $("#inputTest").val(idSelectHumidifier);
+        //Попытки вытянуть опции без кнопки
         // $("#thead_option").after('' +
         //     '<tr className="tr_option" id="tr_option" th:each="instance : ${options.get(' + id + ')}"> ' +
         //         '<td> ' +
@@ -16,10 +23,15 @@ $(document).ready(function(){
         //     '</tr>');
         // $("#tr_option").val(id);
         // $("#table_option").val(id);
-        $("#inputTest").val(id);
         // $(document.createAttribute("idSelectHumidifier",id));
         // $("#tr_option").attr("idSelectHumidifier",id);
         // $("#table_option").attr("idSelectHumidifier",id);
+    });
+});
+$(document).ready(function(){
+    $('input:checkbox[name=selectedOptions]').change(function() {
+        var price = priceHumidifier(idSelectHumidifier) + calcSummaryOption();
+        $("#Summary").text("Сумма: " + price);
     });
 });
 $(document).ready(function(){
@@ -44,3 +56,31 @@ $(document).ready(function(){
         }
     });
 });
+
+function calcSummaryOption() {
+    var table = document.getElementById("table_option");
+    let lastRow = table.rows[table.rows.length-1];
+    let lastCell = lastRow.cells.length-1;
+    var sum=0;
+    for (var i = 1; i < table.rows.length; i++) {
+        let row = table.rows[i];
+            if (row.cells.item(0).getElementsByTagName("input").selectedOptions.checked==true) {
+                sum = sum +
+                    (Number(row.cells.item(lastCell).innerText) || 0);
+            }
+        }
+    return sum;
+}
+
+function priceHumidifier(idSelectHumidifier) {
+    var table = document.getElementById("table_humidifier");
+    let lastRow = table.rows[table.rows.length-1];
+    let lastCell = lastRow.cells.length - 1;
+    for (var i = 1; i < table.rows.length; i++) {
+        let row = table.rows[i];
+        if (row.cells.item(0).getElementsByTagName("input").idSelectHumidifier.getAttribute("value")==idSelectHumidifier) {
+            return (Number(row.cells.item(lastCell).innerText) || 0);
+        }
+    }
+    return 0;
+}
