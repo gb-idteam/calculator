1a26100684618b986c0ef98d433a50aab6564a31
Kirill Kuznetsov <kuznechik.86@mail.ru>
2021-04-15 20:51:46 +0300

    Little Fix

--- src/main/java/ru/systemairac/calculator/controller/MainController.java
+++ src/main/java/ru/systemairac/calculator/controller/MainController.java
@@ -4,7 +4,6 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
--- src/main/java/ru/systemairac/calculator/dto/VaporDistributorDto.java
+++ src/main/java/ru/systemairac/calculator/dto/VaporDistributorDto.java
@@ -8,7 +8,6 @@ import ru.systemairac.calculator.domain.Image;
 
 import java.math.BigDecimal;
 
-@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,6 +17,62 @@ public class VaporDistributorDto {
     private String articleNumber;
     private int length;
     private int diameter;
-    private String title = "Парораспределитель из нержавеющей стали, d=" + this.getDiameter() + "мм, L=" + this.getLength() + "мм";
+    private String title;
     private BigDecimal price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public String getArticleNumber() {
+        return articleNumber;
+    }
+
+    public void setArticleNumber(String articleNumber) {
+        this.articleNumber = articleNumber;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public String getTitle() {
+        return "Парораспределитель из нержавеющей стали, d=" + this.getDiameter() + "мм, L=" + this.getLength() + "мм";
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
